633998316a379c627c6329a3e7f7ec11d0102e1c
Renuah Samuel <renuah1142@gmail.com>
2024-04-19 06:49:07 +0000

    i get tru

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfh6nG1m77Qn8ROmvT1pvsqbT4EN-Cxm4",
+    authDomain: "houseglimpse.firebaseapp.com",
+    projectId: "houseglimpse",
+    storageBucket: "houseglimpse.appspot.com",
+    messagingSenderId: "270740415363",
+    appId: "1:270740415363:web:ba790aeedcc6c5218fccb6",
+    measurementId: "G-02W6V213L7"
+};
+  
+export default firebaseConfig
